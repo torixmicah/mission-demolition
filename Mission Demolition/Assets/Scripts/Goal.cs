972ce3d48dec22b2da36ea8d85d99eeac3089b1e
torixmicah <51023135+torixmicah@users.noreply.github.com>
2021-11-14 17:08:16 +1100

    First commit

---
+++ Mission Demolition/Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // A static field accessible by code anywhere
+    static public bool goalMet = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // When the trigger is hit by something
+        // Check to see if it's a Projectile
+        if (other.gameObject.tag == "Projectile")
+        {
+            // If so, set goalMet to true
+            Goal.goalMet = true;
+            // Also set the alpha of the color to a higher opacity
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = 1;
+            GetComponent<Renderer>().material.color = c;
+
+        }
+    }
+}
